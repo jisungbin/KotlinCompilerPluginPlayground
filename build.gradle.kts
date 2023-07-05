c8593154549ba07c5d2e37797a999cfc7b594fee
jisungbin <ji@sungb.in>
2023-07-05 15:05:04 +0900

    test

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    application
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    google()
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.8.22")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.5.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+}
