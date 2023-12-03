5c5ac7339fe2bcf9f71e3c3fa7fd7f83af34f1bd
jisungbin <ji@sungb.in>
2023-12-03 17:54:20 +0900

    test

--- src/main/kotlin/IrVisitor.kt
+++ src/main/kotlin/IrVisitor.kt
@@ -1,15 +1,8 @@
 import org.jetbrains.kotlin.backend.common.IrElementTransformerVoidWithContext
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.ir.IrStatement
-import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.declarations.IrLocalDelegatedProperty
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.declarations.IrVariable
-import org.jetbrains.kotlin.ir.types.classFqName
-import org.jetbrains.kotlin.ir.types.classOrFail
-import org.jetbrains.kotlin.ir.types.superTypes
 import org.jetbrains.kotlin.ir.util.dump
 import org.jetbrains.kotlin.ir.util.dumpKotlinLike
 import util.Logger
@@ -33,39 +26,5 @@ private class IrVisitor(
     logger("\n\n")
     logger("dumpKotlinLike: ${declaration.dumpKotlinLike()}")
     return super.visitFileNew(declaration)
-  }
-
-  override fun visitVariable(declaration: IrVariable): IrStatement {
-    logger(
-      """
-      [VARIABLE]
-      name: ${declaration.name.asString()}
-      type: ${declaration.type.classFqName?.asString()}
-      super types: ${
-        declaration.type.classOrFail
-          .superTypes()
-          .joinToString(transform = { it.classFqName?.asString() ?: "<unknown>" })
-      }
-      {PROPERTY_DELEGATE}: ${declaration.origin == IrDeclarationOrigin.PROPERTY_DELEGATE}
-      """.trimIndent()
-    )
-    return super.visitVariable(declaration)
-  }
-
-  override fun visitLocalDelegatedProperty(declaration: IrLocalDelegatedProperty): IrStatement {
-    logger(
-      """
-      [LocalDelegatedProperty]
-      name: ${declaration.name.asString()}
-      type: ${declaration.type.classFqName?.asString()}
-      delegate type: ${declaration.delegate.type.classFqName?.asString()}
-      delegate super types: ${
-        declaration.delegate.type.classOrFail
-          .superTypes()
-          .joinToString(transform = { it.classFqName?.asString() ?: "<unknown>" })
-      }
-      """.trimIndent()
-    )
-    return super.visitLocalDelegatedProperty(declaration)
   }
 }
--- src/test/kotlin/source/test.kt
+++ src/test/kotlin/source/test.kt
@@ -1,18 +1,11 @@
 package source
 
-@JvmInline
-value class Lambda(private val action: (value: Any) -> Unit) : Function1<Any, Unit> by action
-
 fun main() {
-  val printer = Lambda { value -> println(value) }
-  printer(0)
-  printer.invoke(1)
+  val result = mutableListOf<Int>()
+  run {
+    val a = 2.also { b: Any ->
+      result.add(b as Int)
+    }
+  }
+  println(result)
 }
-
-//object Test {
-//  var a = 0
-//}
-//
-//fun main() {
-//  println(Test.a)
-//}
