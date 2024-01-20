5bdae816f451f01b9be3ac910401df5e8a44cfc3
jisungbin <ji@sungb.in>
2024-01-20 21:43:37 +0900

    test

--- src/main/kotlin/MainRegistrar.kt
+++ src/main/kotlin/MainRegistrar.kt
@@ -1,14 +1,45 @@
+@file:Suppress("DEPRECATION")
+
 import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
-import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.com.intellij.openapi.extensions.LoadingOrder
+import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import util.Logger
 
-@AutoService(CompilerPluginRegistrar::class)
-class MainRegistrar : CompilerPluginRegistrar() {
-  override val supportsK2 = false
-  override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
+@AutoService(ComponentRegistrar::class)
+class MainRegistrar : ComponentRegistrar {
+  override fun registerProjectComponents(
+    project: MockProject,
+    configuration: CompilerConfiguration,
+  ) {
     val logger = Logger(configuration = configuration)
-    IrGenerationExtension.registerExtension(IrExtension(logger = logger))
+
+    project.extensionArea
+      .getExtensionPoint(IrGenerationExtension.extensionPointName)
+      .registerExtension(
+        object : IrGenerationExtension {
+          override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
+            logger("FIRST IrGenerationExtension")
+          }
+        },
+        LoadingOrder.FIRST,
+        project,
+      )
+
+    project.extensionArea
+      .getExtensionPoint(IrGenerationExtension.extensionPointName)
+      .registerExtension(
+        object : IrGenerationExtension {
+          override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
+            logger("LAST IrGenerationExtension")
+          }
+        },
+        LoadingOrder.LAST,
+        project,
+      )
   }
 }
