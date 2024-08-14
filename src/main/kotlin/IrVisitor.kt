39123cd8d45ba81e78b8df907ec73b64f29138ae
Ji Sungbin <ji@sungb.in>
2024-08-15 04:06:20 +0900

    K2

--- src/main/kotlin/IrVisitor.kt
+++ src/main/kotlin/IrVisitor.kt
@@ -1,19 +1,10 @@
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.name.CallableId
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.ir.util.dump
 
 class IrExtension(private val logger: Logger) : IrGenerationExtension {
   override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
-    val functions = pluginContext.referenceFunctions(CallableId.fromFqName(MUTABLE_LIST_ADD_FQN))
-    logger("Found ${functions.size} functions.")
-  }
-
-  private fun CallableId.Companion.fromFqName(fqName: FqName): CallableId =
-    CallableId(packageName = fqName.parent(), callableName = fqName.shortName())
-
-  companion object {
-    val MUTABLE_LIST_ADD_FQN = FqName("kotlin.collections.MutableList.add")
+    logger(moduleFragment.dump())
   }
 }
--- src/test/kotlin/source/TestSource.kt
+++ src/test/kotlin/source/TestSource.kt
@@ -1,4 +1,18 @@
 package source
 
-@Suppress("unused")
-fun test() = Unit
+fun test() {
+  Any()
+  Any()
+  Any()
+}
+
+fun test2() {
+  when (1 + 1) {
+    2 -> {
+      println("2")
+      println("2")
+      println("2")
+    }
+    else -> println("else")
+  }
+}
