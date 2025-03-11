1f7e1ed99cbb071ae93a9bebce26b4b2ea96e5df
Ji Sungbin <ji@sungb.in>
2025-03-11 12:56:41 +0900

    test

--- src/test/kotlin/source/TestSource.kt
+++ src/test/kotlin/source/TestSource.kt
@@ -1,5 +1,16 @@
 package source
 
-fun plus(a: Int, b: Int) {
-  println(a + b)
+private fun currentFunctionName(): String = TODO()
+
+fun main() {
+  println(currentFunctionName()) // "main" 출력
+
+  val lambda = { println(currentFunctionName()) }
+  lambda.invoke() // "<anonymous>" 출력
+
+  otherFunction() // "otherFunction" 출력
+}
+
+fun otherFunction() {
+  println(currentFunctionName())
 }
