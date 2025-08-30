aa336b09416bbc02215201e2b3bc51a8122f8a91
jisungbin <ji@sungb.in>
2025-08-30 13:51:39 +0900

    test

--- src/main/kotlin/IrVisitor.kt
+++ src/main/kotlin/IrVisitor.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.expressions.IrLocalDelegatedPropertyReference
+import org.jetbrains.kotlin.ir.util.dump
 import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 
@@ -18,8 +19,7 @@ class IrTestExtension(private val logger: Logger) : IrGenerationExtension {
 
         override fun visitLocalDelegatedPropertyReference(expression: IrLocalDelegatedPropertyReference) {
           super.visitLocalDelegatedPropertyReference(expression)
-//          logger("LocalDelegatedPropertyReference: ${expression.dump()}")
-          logger("LocalDelegatedPropertyReference: ${expression.dumpSrc(useFir = true)}")
+          logger("LocalDelegatedPropertyReference: ${expression.dumpSrc(true)}")
         }
       }
     )
--- src/test/kotlin/source/TestSource.kt
+++ src/test/kotlin/source/TestSource.kt
@@ -2,12 +2,8 @@ package source
 
 import kotlin.properties.Delegates
 
-
 fun main() {
-//  var a by Delegates.notNull<Int>()
-//  a = 1
-//  println(a)
-
-  val a = Any()
-  ::a
+  var a by Delegates.notNull<Int>()
+  a = 1
+  println(a)
 }
