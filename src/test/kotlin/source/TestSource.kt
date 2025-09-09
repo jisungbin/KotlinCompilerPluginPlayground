9e0d41aeece19bcc40b97ce11048762455221a46
jisungbin <ji@sungb.in>
2025-09-09 22:03:37 +0900

    test

--- src/test/kotlin/source/TestSource.kt
+++ src/test/kotlin/source/TestSource.kt
@@ -1,6 +1,9 @@
 package source
 
-class T(
-  private val a: Int,
-  b: Int,
-)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class T {
+  var a: Any by mutableStateOf(Any())
+}
